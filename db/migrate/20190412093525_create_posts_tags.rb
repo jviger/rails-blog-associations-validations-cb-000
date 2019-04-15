045fad3e2c91afde1e35cdc1e822a5711ce194b1
Jason Viger <jason.viger@gmail.com>
2019-04-15 04:06:53 +0000

    Automatically backed up by Learn

--- db/migrate/20190412093525_create_posts_tags.rb
+++ db/migrate/20190412093525_create_posts_tags.rb
@@ -1,10 +1,9 @@
-class CreatePosts < ActiveRecord::Migration
+class CreatePostsTags < ActiveRecord::Migration[5.2]
   def change
-    create_table :posts do |t|
+    create_table :posts_tags do |t|
       t.string :name
       t.text :content
       t.timestamps null: false
       t.integer :user_id
-    end
   end
 end
